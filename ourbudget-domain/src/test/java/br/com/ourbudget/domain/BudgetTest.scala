d6d8921c1f1b402840ff4adfbcd23d915cad7c9f
Robson Duarte <robson.o.d@gmail.com>
2015-05-19 23:32:27 -0300

    add simple test for budget

--- ourbudget-domain/src/test/java/br/com/ourbudget/domain/BudgetTest.scala
+++ ourbudget-domain/src/test/java/br/com/ourbudget/domain/BudgetTest.scala
@@ -6,8 +6,12 @@ import org.scalatest.Matchers
 class BugdetTest extends FlatSpec with Matchers  {
 
   "one test" should "for exmple" in {
-    
+      var bugdet = new Bugdet("test", 2.22)
+      
+      bugdet.name should be ("test")
+      bugdet.value should be (2.22)
+       
   }
-  
+
   
 }
