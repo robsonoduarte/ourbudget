ca9e6247a64848660d311e3d6dd34aa628299962
Robson Duarte <robson.o.d@gmail.com>
2016-04-08 16:44:32 -0300

    back to mavem ( SBT ) will use in the future ( I need learn first ).

--- ourbudget-domain/src/main/scala/br/com/ourbudget/domain/Mongo.scala
+++ ourbudget-domain/src/main/scala/br/com/ourbudget/domain/Mongo.scala
@@ -1,16 +1,16 @@
 package br.com.ourbudget.domain
 
-import com.mongodb.casbah.MongoClient
+/*import com.mongodb.casbah.MongoClient
 import com.mongodb.casbah.commons.MongoDBObject
 import com.typesafe.config.ConfigFactory
-
+*/
 
 /**
  * Created by Thiago Pereira on 7/24/15.
  */
 object Mongo extends App {
 
-  val config = ConfigFactory.load()
+  /*val config = ConfigFactory.load()
 
   val mongoClient = MongoClient(config.getString("mongo.client"))
   val db =  mongoClient(config.getString("mongo.db"))
@@ -21,5 +21,5 @@ object Mongo extends App {
 
   coll.insert(a)
 
-  db.collectionNames() foreach print
+  db.collectionNames() foreach print*/
 }
