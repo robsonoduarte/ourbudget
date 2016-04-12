ec2a24eb8baf0c0dfdea58d9b0f3aa29a7979f12
Robson Duarte <robson.o.d@gmail.com>
2016-04-11 21:38:21 -0300

    add id var ( private ), and toString ( print json )

--- ourbudget-domain/src/main/scala/br/com/ourbudget/domain/Budget.scala
+++ ourbudget-domain/src/main/scala/br/com/ourbudget/domain/Budget.scala
@@ -1,12 +1,27 @@
 package br.com.ourbudget.domain
 
+import org.mongodb.morphia.annotations.Entity
+import com.google.gson.Gson
+import org.mongodb.morphia.annotations.Id
+import org.bson.types.ObjectId
 
 
-class Budget(val name: String, val revenues: List[Revenue] = List()) {
 
-  def addRevenue(revenue: Revenue): Budget = {
-    val copy = new Budget(name, revenues.::(revenue) )
+
+
+class Budget(val name: String, val revenues: Array[Revenue] = Array()) {
+
+  private var id : String = null
+
+
+  def addRevenue(revenue: Revenue) = {
+    val copy = new Budget(name, revenues.+:(revenue) )
+    copy.id = id
     copy
   }
+
+
+
+  override def toString = new Gson().toJson(this)
 
 }
