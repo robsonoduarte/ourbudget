8618606a5cce70652d94a4d4f7053e3b7e61e541
Robson Duarte <robson.o.d@gmail.com>
2016-04-19 20:44:06 -0300

    change the name of module to ourbudget-api , remove suport to wildflysarwm for use scalantra.

--- ourbudget-persistence-api/src/main/java/br/com/ourbudget/persistence/api/ApplicationConfig.java
+++ ourbudget-persistence-api/src/main/java/br/com/ourbudget/persistence/api/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package br.com.ourbudget.persistence.api;
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
-public class ApplicationConfig extends Application {}
+
+
+public class ApplicationConfig {}
--- ourbudget-persistence-api/src/main/scala/br/com/ourbudget/persistence/api/OurBudget.scala
+++ ourbudget-persistence-api/src/main/scala/br/com/ourbudget/persistence/api/OurBudget.scala
@@ -1,20 +1,12 @@
 package br.com.ourbudget.persistence.api
 
 import br.com.ourbudget.domain.Budget
-import javax.ws.rs.Path
-import javax.ws.rs.POST
 
 
-@Path("/ourbudget")
 class OurBudget {
 
 
-  @POST
   def add(budget: String) = {
-    print("add")
   }
-
-
-
 
 }
