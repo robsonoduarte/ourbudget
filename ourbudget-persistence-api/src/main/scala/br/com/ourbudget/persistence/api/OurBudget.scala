a99d75740520d4b2c1061aee2d10067773c40fcc
Robson Duarte <robson.o.d@gmail.com>
2016-04-25 21:31:16 -0300

    starting test suport of format json with scalantra

--- ourbudget-persistence-api/src/main/scala/br/com/ourbudget/persistence/api/OurBudget.scala
+++ ourbudget-persistence-api/src/main/scala/br/com/ourbudget/persistence/api/OurBudget.scala
@@ -4,6 +4,11 @@ import br.com.ourbudget.domain.Budget
 import org.scalatra._
 
 
+
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
+
+
 class OurBudget extends ScalatraServlet{
 
 
