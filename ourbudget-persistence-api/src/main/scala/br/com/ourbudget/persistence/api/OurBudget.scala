ebb0f66072bb18b147ed6eb3c2a56b4c9a40d575
Robson Duarte <robson.o.d@gmail.com>
2016-04-27 21:31:31 -0300

    add simple test of integration api with persistence.

--- ourbudget-persistence-api/src/main/scala/br/com/ourbudget/persistence/api/OurBudget.scala
+++ ourbudget-persistence-api/src/main/scala/br/com/ourbudget/persistence/api/OurBudget.scala
@@ -7,11 +7,15 @@ import org.scalatra._
 
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json._
+import br.com.ourbudget.persistence.mongo.Repository
 
 
 class OurBudget extends ScalatraServlet with JacksonJsonSupport{
 
    protected implicit lazy val jsonFormats: Formats = DefaultFormats
+
+
+   var repo = new Repository
 
 
   before(){
@@ -25,7 +29,8 @@ class OurBudget extends ScalatraServlet with JacksonJsonSupport{
 
   post("/add"){
     	var budget = parsedBody.extract[Budget]
-    	print(budget)
+    	var x = repo save budget
+    	x
   }
 
 
