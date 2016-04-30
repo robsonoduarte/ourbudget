117ae8946f00ce5864833a5e4c3ec903564437bf
Robson Duarte <robson.o.d@gmail.com>
2016-04-29 23:01:38 -0300

    add find ( id , class )  method in repository.

--- ourbudget-persistence-mongo/src/main/scala/br/com/ourbudget/persistence/mongo/Repository.scala
+++ ourbudget-persistence-mongo/src/main/scala/br/com/ourbudget/persistence/mongo/Repository.scala
@@ -2,6 +2,8 @@ package br.com.ourbudget.persistence.mongo
 
 import org.springframework.data.mongodb.core.MongoTemplate
 import com.mongodb.Mongo
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.data.mongodb.core.query.Criteria._
 
 class Repository {
 
@@ -10,8 +12,10 @@ class Repository {
 
 
 
-  def save[B](b: B) = mongoOps.save(b)
+  def save[B](b: B) = mongoOps save(b)
 
+
+  def find[B](id:String, c: Class[B] ): B = mongoOps findOne(new Query(where("id").is(id)), c)
 
 
 }
--- ourbudget-persistence-mongo/src/test/scala/br/com/ourbudget/persistence/mongo/RepositorySpec.scala
+++ ourbudget-persistence-mongo/src/test/scala/br/com/ourbudget/persistence/mongo/RepositorySpec.scala
@@ -11,14 +11,21 @@ import br.com.ourbudget.domain.Budget
 class RepositorySpec extends FlatSpec with Matchers {
 
 
+	val repository = new Repository
 
 
   // simple test
 
   "it" should "save the object" in {
-    val repository = new Repository
-
     repository save(new Budget("test"))
+  }
+
+
+
+  "it" should "find object by id" in {
+		val budget = repository find("572405504c7439ab86008716", classOf[Budget])
+
+		budget shouldBe a [Budget]
   }
 
 
