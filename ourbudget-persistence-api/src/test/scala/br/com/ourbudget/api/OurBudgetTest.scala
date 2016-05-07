fcde53316766f2ef22422c6b7705b434770bf1f5
Robson Duarte <robson.o.d@gmail.com>
2016-05-06 23:08:57 -0300

    refactor the tests with suport of json4s.

--- ourbudget-persistence-api/src/test/scala/br/com/ourbudget/api/OurBudgetTest.scala
+++ ourbudget-persistence-api/src/test/scala/br/com/ourbudget/api/OurBudgetTest.scala
@@ -10,24 +10,43 @@ import br.com.ourbudget.domain.Revenue
 import br.com.ourbudget.domain.Expenditure
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
+import java.sql.Date
 
 class OurBudgetTests extends ScalatraSuite with FunSuiteLike {
 
+  implicit val formats = DefaultFormats
+
+
   addServlet(classOf[OurBudget], "/*")
 
-
-
-  // TODO: for continue the tests, we will add support of JacksonJson maybe with trait... ( searching first )
 
 
   val budget = "{\"name\": \"Travel\"}"
 
    test("should create new Budget and return json with the id"){
      post("/new", budget ){
+
        status should equal(200)
-       body should startWith ("{\"name\":\"Travel\",\"revenues\":[],\"expenditures\":[],\"id\":")
+
+       val json = parse(body)
+
+       (json \ "id").extract[String] should not be empty
+       (json \ "name").extract[String] should  be("Travel")
+
+
+       // validate the format of json
+       json.extract[Budget]
+
      }
+
   }
+
+
+
+
+
+
+
 
 
 
@@ -38,9 +57,43 @@ class OurBudgetTests extends ScalatraSuite with FunSuiteLike {
   test("should add the Revenue in the Budget searching by id"){
      put("/revenue/572414f77e78ea6998893167", revenue ){
        status should equal(200)
-       body should startWith ("{\"name\":\"Travel\",\"revenues\":[{\"name\":\"Salary\",\"value\":400.0}")
+
+
+       val json = parse(body)
+
+
+       (json \ "revenues").extract[Array[Revenue]]
+
+       /*body should startWith ("{\"name\":\"Travel\",\"revenues\":[{\"name\":\"Salary\",\"value\":400.0}")*/
+
      }
+
+
+
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
