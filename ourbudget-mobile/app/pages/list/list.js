9c509b298bb9827ef6c4267177b409e8da4cb464
Robson Duarte <robson.o.d@gmail.com>
2016-05-21 14:59:40 -0300

    start test to integration with api.

--- ourbudget-mobile/app/pages/list/list.js
+++ ourbudget-mobile/app/pages/list/list.js
@@ -18,17 +18,41 @@ export class ListPage {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane', 'american-football', 'boat', 'bluetooth', 'build'];
+    /*this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane', 'american-football', 'boat', 'bluetooth', 'build'];*/
 
-    this.items = [];
 
-    for(let i = 1; i < 11; i++) {
+
+    try{
+    	var rulesRequest = new XMLHttpRequest();
+
+    	rulesRequest.open("GET", "http://192.168.0.3:8080/ourbudget/all", true);
+    	rulesRequest.send()
+
+
+    	this.items = JSON.parse(rulesRequest.responseText);
+    	alert(items)
+
+
+    }catch(err){
+    	 document.getElementById("error").innerHTML = err.message;
+    }
+
+
+
+
+
+/*    for(let i = 1; i < 11; i++) {
       this.items.push({
         title: 'Item ' + i,
         note: 'This is item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
-    }
+    }*/
+
+
+
+
+
   }
 
 
