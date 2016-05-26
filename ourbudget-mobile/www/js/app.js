f46d3b35bc8211d175833c07c59bb460d528c7d6
Robson Duarte <robson.o.d@gmail.com>
2016-05-26 18:29:13 -0300

    comment some states and change some names.

--- ourbudget-mobile/www/js/app.js
+++ ourbudget-mobile/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('ourbudget', ['ionic', 'ourbudget.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -13,7 +13,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
@@ -22,45 +21,51 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
 
+  $stateProvider
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+    	url: '/app',
+    	abstract: true,
+    	templateUrl: 'templates/menu.html',
+    	controller: 'AppCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
+
+  /*.state('app.search', {
+  url: '/search',
+  views: {
+    'menuContent': {
+      templateUrl: 'templates/search.html'
+    }
+  }
+})*/
+
+/*.state('app.browse', {
+    url: '/browse',
     views: {
       'menuContent': {
-        templateUrl: 'templates/search.html'
+        templateUrl: 'templates/browse.html'
       }
     }
-  })
+  }
 
-  .state('app.browse', {
-      url: '/browse',
+)*/
+
+  .state('app.budgets', {
+      url: '/budgets',
+      cache:false,
       views: {
         'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/ourbudgets.html',
-          controller: 'OurBudgetsCtrl'
+          templateUrl: 'templates/budgets.html',
+          controller: 'BudgetsCtrl'
         }
       }
     })
 
   .state('app.single', {
     url: '/playlists/:playlistId',
+    cache:false,
     views: {
       'menuContent': {
         templateUrl: 'templates/playlist.html',
@@ -68,6 +73,9 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   });
+
+
+
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/budgets');
 });
