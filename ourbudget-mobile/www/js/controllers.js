eae10b0f6c9c6004373c3ff71cead55c0bb2f396
Robson Duarte <robson.o.d@gmail.com>
2016-05-27 10:51:06 -0300

    remove the all logic off new budget modal in AppCrtl and put in BudgetsCtrl ( simplify ).

--- ourbudget-mobile/www/js/controllers.js
+++ ourbudget-mobile/www/js/controllers.js
@@ -1,7 +1,6 @@
 angular.module('ourbudget.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout, $http) {
-
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -9,55 +8,38 @@ angular.module('ourbudget.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  // Form data for the login modal
-  $scope.budget = {};
-
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/new.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeNew = function() {
-	  $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.openNew = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doNew = function() {
-    $http.post('http://192.168.0.3:8080/ourbudget/new', $scope.budget)
-	.success(function(result) {
-		$scope.budget = {};
-		$scope.closeNew();
-	})
-	.error(function(data, status) {
-		console.log(status);
-	})
-  };
 })
 
 
-.controller('BudgetsCtrl', function($scope,$http) {
+.controller('BudgetsCtrl', function($scope, $ionicModal, $http){
 
-	$scope.budgets = []
+	$scope.budget = {}
+
+	$ionicModal.fromTemplateUrl('new-budget.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
 
   	$http.get('http://192.168.0.3:8080/ourbudget/all')
-	 .success(function(result) {
-		 $scope.budgets = result;
+  		.success(function(result){
+  			$scope.budgets = result;
 	 })
-	 .error(function(data, status) {
-	 	console.log(data)
-	 })
+
+	 $scope.newBudget = function(budget) {
+		 $http.post('http://192.168.0.3:8080/ourbudget/new', budget)
+			.success(function(result) {
+				$scope.budgets.push(result)
+				$scope.modal.hide()
+				$scope.budget = {}
+		})
+	 }
 })
 
 
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-	console.log($stateParams.id)
+.controller('BudgetCtrl', function($scope, $stateParams) {
+	$scope.id = $stateParams.id
 });
+
