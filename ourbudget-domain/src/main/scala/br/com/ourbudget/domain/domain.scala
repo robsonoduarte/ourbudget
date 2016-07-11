52a5b5add0bc56c3f666bf44d66ca77dd18a7c42
Robso Duarte <robson.o.d@gmail.com>
2016-07-11 20:59:05 -0300

    remove the the world val of constructor.

--- ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -1,12 +1,11 @@
 package br.com.ourbudget.domain
 
 
-case class Revenue(val name: String, val value: Double)
-case class Expenditure(val name: String, val value: Double)
-case class Budget(val id : String = "", val name: String = "",  val revenues: Array[Revenue] = Array(), val expenditures: Array[Expenditure] = Array()){
+case class Revenue(name: String, value: Double)
+case class Expenditure(name: String, value: Double)
+case class Budget(id : String = "", name: String = "",  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array()){
   def +(revenue: Revenue) = copy(id, name, revenues.+:(revenue), expenditures)
   def +(expenditure: Expenditure) = copy(id, name, revenues, expenditures.+:(expenditure))
 }
-
 
 
