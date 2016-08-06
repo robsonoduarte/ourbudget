a626d7ae6b755b7a2b1b721714d03adb6a46f642
Robso Duarte <robson.o.d@gmail.com>
2016-08-05 22:59:10 -0300

    move to User.

--- ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -3,9 +3,13 @@ package br.com.ourbudget.domain
 
 case class Revenue(name: String, value: Double)
 case class Expenditure(name: String, value: Double)
-case class Budget(id : String = "", name: String = "",  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array()){
-  def +(revenue: Revenue) = copy(id, name, revenues.+:(revenue), expenditures)
-  def +(expenditure: Expenditure) = copy(id, name, revenues, expenditures.+:(expenditure))
+case class Budget(id : String = "", name: String = "",  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
+  def +(revenue: Revenue) = copy(id, name, revenues :+ revenue, expenditures)
+  def +(expenditure: Expenditure) = copy(id, name, revenues, expenditures :+ expenditure )
+  def +(user: User) = copy(id, name, revenues, expenditures, users :+ user.id)   
+}
+case class User(id: String = "", name: String = "", budgets: Array[String] = Array()){
+  def +(budget: Budget) = copy(id, name, budgets :+ budget.id)    
 }
 
 
