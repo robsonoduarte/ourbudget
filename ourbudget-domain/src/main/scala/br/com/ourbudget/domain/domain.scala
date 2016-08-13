86e15420e00570bf5db964c8c3a8f760c14dc7b0
Robso Duarte <robson.o.d@gmail.com>
2016-08-13 00:17:55 -0300

    add the balance um budget.

--- ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -3,13 +3,18 @@ package br.com.ourbudget.domain
 
 case class Revenue(name: String, value: Double)
 case class Expenditure(name: String, value: Double)
-case class Budget(id : String = "", name: String = "",  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
-  def +(revenue: Revenue) = copy(id, name, revenues :+ revenue, expenditures)
-  def +(expenditure: Expenditure) = copy(id, name, revenues, expenditures :+ expenditure )
-  def +(user: User) = copy(id, name, revenues, expenditures, users :+ user.id)   
+
+
+// TODO: Study some way to allow create the Budget only with name...
+case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
+  def +(revenue: Revenue) = copy(id, name, balance + revenue.value, revenues :+ revenue, expenditures)
+  def +(expenditure: Expenditure) = copy(id, name, balance - expenditure.value, revenues, expenditures :+ expenditure )
+  def +(user: User) = copy(id, name, balance, revenues, expenditures, users :+ user.id)
 }
+
+
 case class User(id: String = "", name: String = "", budgets: Array[String] = Array()){
-  def +(budget: Budget) = copy(id, name, budgets :+ budget.id)    
+  def +(budget: Budget) = copy(id, name, budgets :+ budget.id)
 }
 
 
