1acea2a8bb6b93a77934a7bc4f81a40b80d13410
Robso Duarte <robson.o.d@gmail.com>
2016-08-19 21:42:06 -0300

    simplify the parameters of copy methods

--- ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-domain/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -7,14 +7,14 @@ case class Expenditure(name: String, value: Double)
 
 // TODO: Study some way to allow create the Budget only with name...
 case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
-  def +(revenue: Revenue) = copy(id, name, balance + revenue.value, revenues :+ revenue, expenditures)
-  def +(expenditure: Expenditure) = copy(id, name, balance - expenditure.value, revenues, expenditures :+ expenditure )
-  def +(user: User) = copy(id, name, balance, revenues, expenditures, users :+ user.id)
+  def +(revenue: Revenue) = copy(balance = balance + revenue.value, revenues = revenues :+ revenue)
+  def +(expenditure: Expenditure) = copy(balance = balance - expenditure.value,  expenditures = expenditures :+ expenditure )
+  def +(user: User) = copy(users = users :+ user.id)
 }
 
 
 case class User(id: String = "", name: String = "", budgets: Array[String] = Array()){
-  def +(budget: Budget) = copy(id, name, budgets :+ budget.id)
+  def +(budget: Budget) = copy(budgets = budgets :+ budget.id)
 }
 
 
