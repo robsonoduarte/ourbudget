a3b31ab99e232948e7eefc0e6a086834b64a7a7f
Robson Duarte <robson.o.d@gmail.com>
2016-09-06 19:05:48 -0300

    immigration of domain to api.

---
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -0,0 +1,20 @@
+package br.com.ourbudget.domain
+
+
+case class Revenue(name: String, value: Double)
+case class Expenditure(name: String, value: Double)
+
+
+// TODO: Study some way to allow create the Budget only with name...
+case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
+  def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ rev)
+  def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ exp )
+  def +(user: User) = copy(users = users :+ user.id)
+}
+
+
+case class User(id: String = "", name: String = "", budgets: Array[String] = Array()){
+  def +(budget: Budget) = copy(budgets = budgets :+ budget.id)
+}
+
+
---
+++ ourbudget-api/src/test/scala/br/com/ourbudget/domain/BudgetSpec.scala
@@ -0,0 +1,75 @@
+package br.com.ourbudget.domain
+
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+
+
+
+class BudgetSpec extends FlatSpec with Matchers {
+
+
+	  val budget = new Budget(name = "Travel")
+
+
+
+	  // REVENUES TESTS
+
+		"it" should "return one new copy of Budget when addd new Revenue" in {
+		  val copy = budget + Revenue("Salary", 100.0)
+		  copy should not be theSameInstanceAs(budget)
+	  }
+
+
+   "it" should "return one new copy of Budget with a new Revenues in List of Revenues" in {
+    	val copy = budget + Revenue("Salary", 100.0)
+      copy.revenues should have length 1
+   }
+
+
+   "it" should "add the Revenues in balance of the Budget" in {
+	   val copy = budget + Revenue("Salary", 100.0)
+	   copy.balance should be (100.0)
+   }
+
+
+
+
+   // EXPENDITURE TESTS
+
+   "it" should "return one new copy of Budget when addd new Expenditure" in {
+		 val copy = budget + Expenditure("Hotel", 100.0)
+		 copy should not be theSameInstanceAs(budget)
+   }
+
+
+   "it" should "return one new copy of Budget with new Expenditure in List of Expenditures " in {
+	   val copy = budget + Expenditure("Hotel", 100.0)
+	   copy.expenditures should have length 1
+   }
+
+   //
+
+   "it" should "subtracting the Expenditure in balance of the Budget" in {
+	   val copy = budget + Expenditure("Hotel", 100.0)
+	   copy.balance should be (-100.0)
+   }
+
+
+
+
+
+
+   // USER TESTS
+
+   "it" should "return on new copy of Budget with the id of User " in {
+     val user = new User(id = "id", name= "Robson")
+	   val copy = budget +  user
+	   copy.users should contain("id")
+   }
+
+
+
+
+
+}
