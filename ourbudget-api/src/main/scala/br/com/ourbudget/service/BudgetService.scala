cfe7493f41b70279f0a73161179ebbe064e04857
Robson Duarte <robson.o.d@gmail.com>
2016-10-07 21:48:27 -0300

    start developing of BudgetService

---
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/BudgetService.scala
@@ -0,0 +1,16 @@
+package br.com.ourbudget.service
+
+import br.com.ourbudget.domain.Budget
+
+class BudgetService {
+  def save(budget: Budget): Budget = null
+}
+
+
+
+
+
+
+object BudgetService{
+  def apply() = new BudgetService()
+}
