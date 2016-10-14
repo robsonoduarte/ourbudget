893a9dff6175ee738466ebd0787acb459450a023
Robson Duarte <robson.o.d@gmail.com>
2016-10-14 18:42:04 -0300

    add + todo.

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -5,8 +5,6 @@ class TODO {
   // TODO:Format the push notification MSG in Popup App
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
-  
-  
-  
+  // TODO: continue the https://github.com/marcuslonnberg/sbt-docker
   
 }
