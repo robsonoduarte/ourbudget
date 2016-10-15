abbbd84d994a52d3035329fe48d5bfc10c02134f
Robson Duarte <robson.o.d@gmail.com>
2016-10-15 10:13:05 -0300

    add append in task to run with sbt.

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -6,7 +6,7 @@ class TODO {
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
   // TODO:continue the https://github.com/marcuslonnberg/sbt-docker
-  // TODO:Jenkins Pipeline local
+  // TODO:Jenkins Pipeline local with SBT
   // TODO:Deploy API in AWS ( Amazon EC2 Container Service  ) 
   // TODO:Docker compose with mongo db
   
