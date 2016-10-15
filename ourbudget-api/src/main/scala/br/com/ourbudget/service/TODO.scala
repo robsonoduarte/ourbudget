73e41c0f0ddf3e17198dc0f4d557529cda411200
Robson Duarte <robson.o.d@gmail.com>
2016-10-15 10:05:05 -0300

    add task to deploy api.

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -5,6 +5,9 @@ class TODO {
   // TODO:Format the push notification MSG in Popup App
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
-  // TODO: continue the https://github.com/marcuslonnberg/sbt-docker
+  // TODO:continue the https://github.com/marcuslonnberg/sbt-docker
+  // TODO:Jenkins Pipeline local
+  // TODO:Deploy API in AWS
+  // TODO:Docker compose with mongo db
   
 }
