94d79a33aa67f12cb439777be928dba61e7d5ae1
Robson Duarte <robson.o.d@gmail.com>
2016-10-29 12:39:18 -0200

    finished tasks and put another

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -1,13 +1,16 @@
 package br.com.ourbudget.service
 
 class TODO {
-  
+
+
+  // TODO: How to connect to a docker container from outside the host (same network)
+  // TODO: AWS Account
+
   // TODO:Format the push notification MSG in Popup App
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
-  // TODO:continue the https://github.com/marcuslonnberg/sbt-docker
   // TODO:Jenkins Pipeline local with SBT
-  // TODO:Deploy API in AWS ( Amazon EC2 Container Service  ) 
-  // TODO:Docker compose with mongo db
-  
+  // TODO:Deploy API in AWS ( Amazon EC2 Container Service  )
+
+
 }
