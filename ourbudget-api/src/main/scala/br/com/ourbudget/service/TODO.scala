a281290da77f6c791360e8d70831f6bb0bc02319
Robson Duarte <robson.o.d@gmail.com>
2016-11-03 16:08:35 -0200

    done some task.

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -10,8 +10,7 @@ class TODO {
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
   // TODO:Jenkins Pipeline local with SBT + deploy in AWS.
-  // TODO:Deploy API in AWS Docker Compose ( API + Mongo )
-  // TODO:Push the image in docke hub
+
   // TODO:Deploy API in AWS ( Amazon EC2 Container Service  )
 
   // TODO: IP Resolver
