848f450d397a595ce9274f65b22ce704cfea56b6
Robson Duarte <robson.o.d@gmail.com>
2016-11-04 17:43:17 -0200

    immigration of tasks to kanbam.

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -3,19 +3,15 @@ package br.com.ourbudget.service
 class TODO {
 
 
-  // TODO: How to connect to a docker container from outside the host (same network)
-
 
   // TODO:Format the push notification MSG in Popup App
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
   // TODO:Jenkins Pipeline local with SBT + deploy in AWS.
 
-  // TODO:Deploy API in AWS ( Amazon EC2 Container Service  )
 
   // TODO: IP Resolver
-
-
   // TODO: authentication the API
+
 
 }
