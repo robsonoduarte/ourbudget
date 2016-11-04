239fa918dd22caf1671b49a7f24d61b24048b967
Robson Duarte <robson.o.d@gmail.com>
2016-11-04 17:46:58 -0200

    move task to kanbam.

--- ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/service/TODO.scala
@@ -7,11 +7,7 @@ class TODO {
   // TODO:Format the push notification MSG in Popup App
   // TODO:Update the data on receive the push notification ( APP opened )
   // TODO:Pattern of data JSON ( SNAPHSHOT )
-  // TODO:Jenkins Pipeline local with SBT + deploy in AWS.
 
-
-  // TODO: IP Resolver
-  // TODO: authentication the API
 
 
 }
