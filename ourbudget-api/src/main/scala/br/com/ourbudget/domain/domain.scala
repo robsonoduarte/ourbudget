79ac12390336f0f86c9e230cda95afd402552d38
Robson Duarte <robson.o.d@gmail.com>
2016-12-05 18:25:23 -0200

    add the attr. closed in Budget.

--- ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -6,7 +6,7 @@ case class Expenditure(name: String, value: Double, tags: Array[String] = Array(
 
 
 // TODO: Study some way to allow create the Budget only with name...
-case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
+case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  closed: Boolean = false, revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
   def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ rev)
   def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ exp )
   def +(user: User) = copy(users = users :+ user.id)
