3526a8e1cad5af93f1c7d97fb39f062307d286c8
Robson Duarte <robson.o.d@gmail.com>
2018-01-26 20:03:29 -0200

    add index attr in Revenue + set index value when add Revenue in Budget

--- ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -1,13 +1,15 @@
 package br.com.ourbudget.domain
 
+import scala.collection.mutable.ArrayBuffer
 
-case class Revenue(name: String, value: Double, received: Boolean = false)
+
+case class Revenue(name: String, value: Double, received: Boolean = false, index: Int = 0)
 case class Expenditure(name: String, value: Double, tags: Array[String] = Array(), liquidated: Boolean = false)
 
 
 // TODO: Study some way to allow create the Budget only with name...
 case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  closed: Boolean = false, revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
-  def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ rev)
+  def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ rev.copy(name = rev.name, value = rev.value, index = revenues.length))
   def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ exp )
   def +(user: User) = copy(users = users :+ user.id)
 }
--- ourbudget-api/src/test/scala/br/com/ourbudget/domain/BudgetSpec.scala
+++ ourbudget-api/src/test/scala/br/com/ourbudget/domain/BudgetSpec.scala
@@ -15,29 +15,48 @@ class BudgetSpec extends FlatSpec with Matchers {
 
 	  // REVENUES TESTS
 
-		"it" should "return one new copy of Budget when addd new Revenue" in {
+	 "it" should "return one new copy of Budget when add new Revenue" in {
 		  val copy = budget + Revenue("Salary", 100.0)
 		  copy should not be theSameInstanceAs(budget)
 	  }
-
 
    "it" should "return one new copy of Budget with a new Revenues in List of Revenues" in {
     	val copy = budget + Revenue("Salary", 100.0)
       copy.revenues should have length 1
    }
-
-
-   "it" should "add the Revenues in balance of the Budget" in {
-	   val copy = budget + Revenue("Salary", 100.0)
-	   copy.balance should be (100.0)
+   
+   
+   "it" should "return one new copy of Budget with a new Revenues with index" in {
+	   val copy = budget + Revenue("Salary", 100.0) + Revenue("Investiments", 100.0)
+	   copy.revenues should have length 2
+	   copy.revenues(0).index should be(0)
+	   copy.revenues(1).index should be(1)
    }
 
 
+   "it" should "add the Revenues in balance of the Budget" in {
+	   val copy = budget + Revenue("Salary", 100.0) + Revenue("Investiments", 100.0)
+	   copy.balance should be (200.0)
+   }
+
+
+/*   "it" should "return one new copy of Budget when remove one Revenue" in {
+		  val copy = budget - Revenue("Hotel", 100.0)
+		  copy should not be theSameInstanceAs(budget)
+	  }
+*/
+
+
+   
+   
+   
+   
+   
 
 
    // EXPENDITURE TESTS
 
-   "it" should "return one new copy of Budget when addd new Expenditure" in {
+   "it" should "return one new copy of Budget when add new Expenditure" in {
 		 val copy = budget + Expenditure("Hotel", 100.0)
 		 copy should not be theSameInstanceAs(budget)
    }
@@ -50,10 +69,10 @@ class BudgetSpec extends FlatSpec with Matchers {
 
    //
 
-   "it" should "subtracting the Expenditure in balance of the Budget" in {
+  /* "it" should "subtracting the Expenditure in balance of the Budget" in {
 	   val copy = budget + Expenditure("Hotel", 100.0)
 	   copy.balance should be (-100.0)
-   }
+   }*/
 
 
 
