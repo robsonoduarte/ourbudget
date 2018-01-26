8b478d4d06584265be6418430f7e47e95d87436e
Robson Duarte <robson.o.d@gmail.com>
2018-01-26 20:28:37 -0200

    add the attr index in Expenditure + set index whe add Expenditure in Budget

--- ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -4,14 +4,19 @@ import scala.collection.mutable.ArrayBuffer
 
 
 case class Revenue(name: String, value: Double, received: Boolean = false, index: Int = 0)
-case class Expenditure(name: String, value: Double, tags: Array[String] = Array(), liquidated: Boolean = false)
+case class Expenditure(name: String, value: Double, tags: Array[String] = Array(), liquidated: Boolean = false, index: Int = 0)
 
 
 // TODO: Study some way to allow create the Budget only with name...
 case class Budget(id : String = "", name: String = "", balance: Double = 0.0,  closed: Boolean = false, revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
-  def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ rev.copy(name = rev.name, value = rev.value, index = revenues.length))
-  def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ exp )
+  def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ _copy(rev))
+  def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ _copy(exp) )
   def +(user: User) = copy(users = users :+ user.id)
+  
+ 
+  private def _copy(rev: Revenue) = rev.copy(name = rev.name, value = rev.value, index = revenues.length)
+  private def _copy(exp: Expenditure) = exp.copy(name=exp.name, value=exp.value, tags=exp.tags, index = expenditures.length)
+  
 }
 
 
--- ourbudget-api/src/test/scala/br/com/ourbudget/domain/BudgetSpec.scala
+++ ourbudget-api/src/test/scala/br/com/ourbudget/domain/BudgetSpec.scala
@@ -26,7 +26,7 @@ class BudgetSpec extends FlatSpec with Matchers {
    }
    
    
-   "it" should "return one new copy of Budget with a new Revenues with index" in {
+   "it" should "return one new copy of Budget with the new Revenues with index" in {
 	   val copy = budget + Revenue("Salary", 100.0) + Revenue("Investiments", 100.0)
 	   copy.revenues should have length 2
 	   copy.revenues(0).index should be(0)
@@ -66,13 +66,20 @@ class BudgetSpec extends FlatSpec with Matchers {
 	   val copy = budget + Expenditure("Hotel", 100.0)
 	   copy.expenditures should have length 1
    }
+   
+   
+   "it" should "return one new copy of Budget with the new Expenditure with index" in {
+	   val copy = budget + Expenditure("Hotel", 100.0) + Expenditure("Airplane", 100.0)
+	   copy.expenditures(0).index should be(0)
+	   copy.expenditures(1).index should be(1)
+   }
 
-   //
+   
 
-  /* "it" should "subtracting the Expenditure in balance of the Budget" in {
-	   val copy = budget + Expenditure("Hotel", 100.0)
-	   copy.balance should be (-100.0)
-   }*/
+  "it" should "subtracting the Expenditure in balance of the Budget" in {
+	   val copy = budget + Expenditure("Hotel", 100.0) + Expenditure("Airplane", 100.0)
+	   copy.balance should be (-200.0)
+   }
 
 
 
