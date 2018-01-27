36a79019f92e74e9034f1675fd8d6aedcbf0ea32
Robson Duarte <robson.o.d@gmail.com>
2018-01-26 23:29:54 -0200

    Use the Budget in test.

--- ourbudget-api/src/main/scala/br/com/ourbudget/domain/Temp.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/Temp.scala
@@ -15,30 +15,27 @@ object Temp extends App {
   //save
 
 
-  val temp = repo.find("5932d765f4ea63cf8c17c546", classOf[Temp])
+  val budget = repo.find("5932d765f4ea63cf8c17c546", classOf[Budget])
 
 //  val z = temp + new Revenue("Vale Ana", 2.0)
   //repo.save( z)
   //repo.save( z + new Revenue("Vale Ana", 2.0) )
 
-  //val x = temp - new Revenue("Salario Robson", 8.0)
- // val x = temp update new Revenue(name= "Emprestimo Rosana",  value = 10.0, true, 5)
-  val x = temp - Revenue(name= "Resgate LCA",  value = 10.0, true, 4)
+   val copy = budget - new Revenue("Salario Robson", 8.0)
+  // val x = temp update new Revenue(name= "Emprestimo Rosana",  value = 10.0, true, 0)
+  //val x = temp - Revenue(name= "Resgate LCA",  value = 10.0, true, 4)
 
-
-  repo.save(x)
+  repo.save(copy)
 
 
   def save {
-    val x = Array(Revenue("Salario Robson",8.0),Revenue("Salario Ana",8.0))
-    repo.save(Temp("5932d765f4ea63cf8c17c546",x))
+    repo.save(Budget("5932d765f4ea63cf8c17c546", "Test") + Revenue(name = "Salario Robson", value = 8.0) + Revenue("Salario Ana",8.0))
   }
 
 }
 
 
-//case class Temp(id: String, revenues: Array[Revenue])
-case class Temp(id: String, revenues: Array[Revenue]){
+/*case class Temp(id: String, revenues: Array[Revenue]){
   def +(rev: Revenue) = copy(revenues = revenues :+ rev.copy(rev.name, rev.value,rev.received, revenues.size))
   def -(rev: Revenue) = copy(revenues = temp(rev) )
   def update(rev: Revenue)= copy( revenues = revenues.updated(rev.index, rev.copy(rev.name, rev.value, rev.received)))
@@ -47,5 +44,4 @@ case class Temp(id: String, revenues: Array[Revenue]){
     val x = revenues.filter(! _.equals(rev))
     x.map(y => y.copy(index = (x.indexOf(y)))).toArray
   }
-
-}
+}*/
