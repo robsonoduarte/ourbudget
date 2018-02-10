18f391baac0140c7e60d7c60fce5b6f8c240d979
Robson Duarte <robson.o.d@gmail.com>
2018-02-09 22:46:01 -0200

    add mongo template buffer

---
+++ ourbudget-api/src/main/scala/br/com/ourbudget/repo/MongoTemplateBuffer.scala
@@ -0,0 +1,17 @@
+package br.com.ourbudget.repo
+
+import org.springframework.data.mongodb.core.MongoTemplate
+import com.mongodb.Mongo
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+
+object MongoTemplateBuffer {
+
+    private val MONGO_URI = "mongodb://@cluster0-shard-00-00-oy673.mongodb.net:27017,cluster0-shard-00-01-oy673.mongodb.net:27017,cluster0-shard-00-02-oy673.mongodb.net:27017/admin?replicaSet=Cluster0-shard-0&ssl=true"      
+    private val mongoTemplate: MongoTemplate = new MongoTemplate( new MongoClient(new MongoClientURI(MONGO_URI)), "OURBUDGET-PRD")
+    
+     
+    def get = mongoTemplate
+  
+ 
+}
---
+++ ourbudget-api/src/test/scala/br/com/ourbudget/repo/MongoTemplateBufferSpec.scala
@@ -0,0 +1,22 @@
+package br.com.ourbudget.repo
+
+import java.util.Arrays.asList
+import com.mongodb.ServerAddress
+import org.scalatest.FlatSpec
+import org.scalatest.Matchers
+
+class MongoTemplateBufferSpec extends FlatSpec with Matchers{
+
+
+
+	it should "return the MongoTemplate  same Mongo in all instances" in {
+
+		var mongo1 = MongoTemplateBuffer.get
+		var mongo2 = MongoTemplateBuffer.get
+
+		mongo1.getDb.getMongo should equal(mongo2.getDb.getMongo)
+
+	}
+	
+
+}
