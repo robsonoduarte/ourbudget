d2abd8c053fd06f10c211d548166cd2fe7a6e0f3
Robson Duarte <robson.o.d@gmail.com>
2018-02-09 22:45:34 -0200

    refactor repo to use MongoTemplateBuffer

--- ourbudget-api/src/main/scala/br/com/ourbudget/repo/Repo.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/repo/Repo.scala
@@ -3,27 +3,21 @@
 package br.com.ourbudget.repo
 
 import org.springframework.data.mongodb.core.MongoTemplate
-import com.mongodb.Mongo
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Criteria._
 import scala.collection.JavaConversions._
-import com.mongodb.ServerAddress
 import br.com.ourbudget.domain.Budget
-
 
 
 class Repo {
 
 
-  val mongoOps = new MongoTemplate(new Mongo(servers), "ourbudget")
+  private val mongo = MongoTemplateBuffer.get
 
-  def save[B](b: B) = mongoOps save(b)
-  def find[B](id:String, c: Class[B] ) = mongoOps findOne(new Query(where("id").is(id)), c)
-  def list[B](c: Class[B]): List[B] = mongoOps.findAll(c).toList
-  def listToUser(id: String) = mongoOps.find(new Query(where("users").in(id)), classOf[Budget]).toList
-
-
-  def servers = List(new ServerAddress("172.17.0.2",27017))
+  def save[B](b: B) = mongo save(b)
+  def find[B](id:String, c: Class[B] ) = mongo findOne(new Query(where("id").is(id)), c)
+  def list[B](c: Class[B]): List[B] = mongo.findAll(c).toList
+  def listToUser(id: String) = mongo.find(new Query(where("users").in(id)), classOf[Budget]).toList
 
 }
 
