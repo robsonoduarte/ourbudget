461d3d5efb08e3feafb58c53ef92d4e0acb50f4f
Robson Duarte <robson.o.d@gmail.com>
2018-02-23 16:15:53 -0300

    rename the method + and - to :+ and :-

--- ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -9,8 +9,8 @@ case class Expenditure(name: String, value: Double, category: String, liquidated
 
 // TODO: Study some way to allow create the Budget only with name...
 case class Budget(id : String = "", name: String, balance: Double = 0.0,  closed: Boolean = false, revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
-  def +(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ _copy(rev))
-  def -(rev: Revenue) = copy( balance = --(rev),  revenues = remove(rev))
+  def :+(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ _copy(rev))
+  def :-(rev: Revenue) = copy( balance = --(rev),  revenues = remove(rev))
   def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ _copy(exp) )
   def -(exp: Expenditure) = copy(balance = ++(exp), expenditures = remove(exp))
   def +(user: User) = copy(users = users :+ user.id)
