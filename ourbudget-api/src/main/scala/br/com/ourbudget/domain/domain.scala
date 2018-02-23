e1cd78a26cdcecad774c7e334154c22c0662b46e
Robson Duarte <robson.o.d@gmail.com>
2018-02-23 17:28:03 -0300

    finished refactor method :- of Budget

--- ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/domain/domain.scala
@@ -10,7 +10,24 @@ case class Expenditure(name: String, value: Double, category: String, liquidated
 // TODO: Study some way to allow create the Budget only with name...
 case class Budget(id : String = "", name: String, balance: Double = 0.0,  closed: Boolean = false, revenues: Array[Revenue] = Array(), expenditures: Array[Expenditure] = Array(), users : Array[String] = Array()){
   def :+(rev: Revenue) = copy(balance = balance + rev.value, revenues = revenues :+ _copy(rev))
-  def :-(rev: Revenue) = copy( balance = --(rev),  revenues = remove(rev))
+  
+//  def :-(index: Int) = copy( /*balance = --(rev), */ revenues = ::-(index))
+  
+  def :-(index: Int) = copy( 
+      balance =  {
+        try {          
+        	balance - revenues(index).value
+        } catch {
+          case t: IndexOutOfBoundsException => balance
+        }        
+      },                
+      revenues = {
+          val revs = revenues.filter(_.index != index)          
+          revs.map(r => r.copy(index = (revs.indexOf(r)))).toArray
+      }
+   )
+  
+  
   def +(exp: Expenditure) = copy(balance = balance - exp.value,  expenditures = expenditures :+ _copy(exp) )
   def -(exp: Expenditure) = copy(balance = ++(exp), expenditures = remove(exp))
   def +(user: User) = copy(users = users :+ user.id)
@@ -19,14 +36,14 @@ case class Budget(id : String = "", name: String, balance: Double = 0.0,  closed
   private def _copy(rev: Revenue) = rev.copy(name = rev.name, value = rev.value, index = revenues.length)
   private def _copy(exp: Expenditure) = exp.copy(name = exp.name, value = exp.value, category = exp.category, index = expenditures.length)
 
-  private def --(rev: Revenue) = if(revenues.contains(rev)) balance - rev.value else balance
+  
   private def ++(exp: Expenditure) = if(expenditures.contains(exp)) balance + exp.value else balance
 
 
-  private def remove(rev: Revenue) = {
-    val revs = revenues.filter(! _.equals(rev))
+  /*private def ::-(index: Int) = {
+    val revs = revenues.filter(_.index == index)
     revs.map(r => r.copy(index = (revs.indexOf(r)))).toArray
-  }
+  }*/
   
   private def remove(exp: Expenditure) = {
 	val exps = expenditures.filter(! _.equals(exp))
