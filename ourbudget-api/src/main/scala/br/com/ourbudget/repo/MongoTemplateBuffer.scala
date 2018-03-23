6b68706ece30d014e98a8955a5253a0a664b7a78
Robson Duarte <robson.o.d@gmail.com>
2018-03-23 19:45:56 -0300

    format code.

--- ourbudget-api/src/main/scala/br/com/ourbudget/repo/MongoTemplateBuffer.scala
+++ ourbudget-api/src/main/scala/br/com/ourbudget/repo/MongoTemplateBuffer.scala
@@ -7,7 +7,7 @@ import com.mongodb.MongoClientURI
 
 object MongoTemplateBuffer {
 
-    private val MONGO_URI = "mongodb://robsonoduarte:Anamara%40123@cluster0-shard-00-00-oy673.mongodb.net:27017,cluster0-shard-00-01-oy673.mongodb.net:27017,cluster0-shard-00-02-oy673.mongodb.net:27017/admin?replicaSet=Cluster0-shard-0&ssl=true"      
+    private val MONGO_URI = "mongodb://@cluster0-shard-00-00-oy673.mongodb.net:27017,cluster0-shard-00-01-oy673.mongodb.net:27017,cluster0-shard-00-02-oy673.mongodb.net:27017/admin?replicaSet=Cluster0-shard-0&ssl=true"      
     private val mongoTemplate: MongoTemplate = new MongoTemplate( new MongoClient(new MongoClientURI(MONGO_URI)), "OURBUDGET-DEV")
     
      
