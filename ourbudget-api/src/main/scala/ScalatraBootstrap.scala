b348e117df31bcced305aaac1058a7d40da3d8dd
Robson Duarte <robson.o.d@gmail.com>
2018-04-06 20:28:03 -0300

    format code.

--- ourbudget-api/src/main/scala/ScalatraBootstrap.scala
+++ ourbudget-api/src/main/scala/ScalatraBootstrap.scala
@@ -7,6 +7,7 @@ class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext){
     context mount(new OurBudgetController, "/ourbudget/api/v1/*", "ourbudget")
   }
+  
 }
 
 
